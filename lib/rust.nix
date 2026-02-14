c7c14c6b5354ae9fa1375729d18b50b5b0bcc1ad
Jacob Sigling <jacob@sigling.se>
2026-02-14 23:07:45 +0100

    Refactored `rust.nix` to be per each system.

--- lib/rust.nix
+++ lib/rust.nix
@@ -1,38 +1,37 @@
 { nixpkgs, flake-utils, rust-overlay }:
-flake-utils.lib.eachDefaultSystem (system:
-  let
-    overlays = [ (import rust-overlay) ];
-    pkgs = import nixpkgs {
-      config.allowUnfree = true;
-      inherit system overlays;
+system:
+let
+  overlays = [ (import rust-overlay) ];
+  pkgs = import nixpkgs {
+    config.allowUnfree = true;
+    inherit system overlays;
+  };
+  rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+    extensions = [ "rust-src" ];
+  };
+in
+{
+  # Function to create a Rust dev shell with additional packages
+  mkRustShell = { extraBuildInputs ? [], extraNativeBuildInputs ? [], extraEnv ? {}, shellHook ? "" }:
+    with pkgs; mkShell rec {
+      nativeBuildInputs = [
+        pkg-config
+      ] ++ extraNativeBuildInputs;
+
+      buildInputs = [
+        rustToolchain
+        bash
+        udev
+        openssl
+      ] ++ extraBuildInputs;
+
+      #Environment Variables
+      LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
+    } // extraEnv // {
+      shellHook = shellHook;
     };
-    rustToolchain = pkgs.rust-bin.stable.latest.default.override {
-      extensions = [ "rust-src" ];
-    };
-  in
-  {
-    # Function to create a Rust dev shell with additional packages
-    mkRustShell = { extraBuildInputs ? [], extraNativeBuildInputs ? [], extraEnv ? {}, shellHook ? "" }:
-      with pkgs; mkShell rec {
-        nativeBuildInputs = [
-          pkg-config
-        ] ++ extraNativeBuildInputs;
 
-        buildInputs = [
-          rustToolchain
-          bash
-          udev
-          openssl
-        ] ++ extraBuildInputs;
-
-        #Environment Variables
-        LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
-      } // extraEnv // {
-        shellHook = shellHook;
-      };
-
-    # Export useful items
-    inherit rustToolchain;
-    inherit pkgs;
-  }
-)
+  # Export useful items
+  inherit rustToolchain;
+  inherit pkgs;
+}
