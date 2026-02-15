b7bcff6e7b5a56dce7a4fc3d55351d62a0a205f2
Jacob Sigling <jacob@sigling.se>
2026-02-15 10:48:05 +0100

    Bumped system, nixpkgs, and home-manager versions to 25.11.

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "Personal Nix configurations and flake libraries";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     flake-utils.url = "github:numtide/flake-utils";
--- home/siglaz/common.nix
+++ home/siglaz/common.nix
@@ -1,7 +1,7 @@
 { pkgs, config, ... }:
 
 {
-  home.stateVersion = "25.05";
+  home.stateVersion = "25.11";
 
   home.sessionPath = [
     "${config.home.homeDirectory}/.local/share/JetBrains/Toolbox/scripts"
